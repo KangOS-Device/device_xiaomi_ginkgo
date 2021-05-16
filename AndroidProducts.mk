3714b616e44e644658ce1ba1ae13581ae8079b94
DefaultioX <DavidYonatan9@hotmail.com>
2021-05-16 01:19:37 +0000

    ginkgo: Bringup KangOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2020 ArrowOS
+# Copyright (C) 2020 The KangOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_ginkgo.mk
+    $(LOCAL_DIR)/kangos_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_ginkgo-user \
-    arrow_ginkgo-userdebug \
-    arrow_ginkgo-eng
+    kangos_ginkgo-user \
+    kangos_ginkgo-userdebug \
+    kangos_ginkgo-eng
